cc7a23e6cafdcba5826e4af88dfb2c3711300816
Amine-youcode1 <bourhymamine@gmail.com>
2024-09-25 12:13:36 +0200

    Challenge 2

---
+++ challeng2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+ char caracter;
+ 
+ printf("entrer un caracter: ");
+ scanf("%s",&caracter);
+ switch (caracter) {
+     case 'a':
+     printf("l'utilisateur est une voyelle");
+     break;
+      case 'i':
+     printf("l'utilisateur est une voyelle");
+     break;
+      case 'e':
+     printf("l'utilisateur est une voyelle");
+     break;
+      case 'u':
+     printf("l'utilisateur est une voyelle");
+     break;
+      case 'o':
+     printf("l'utilisateur est une voyelle");
+     break;
+      default :
+      printf("l'utilisateur est non voyelle");
+      
+     
+ }
