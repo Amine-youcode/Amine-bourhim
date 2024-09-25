5c4f0ebea93962ca4603b4e84fcc56733a1f6eec
Amine-youcode1 <bourhymamine@gmail.com>
2024-09-25 17:10:10 +0200

    CHALLENGE7

---
+++ CHALLENGE7.c
@@ -0,0 +1,17 @@
+#include <math.h>
+int main() {
+  int a;
+  printf("entrer la lettre: ");
+  scanf("%i",&a);
+  if ('A'>= a && a>='Z'){
+      printf("la lettre est majiscule");
+  }
+  else {
+      printf("la lettre est miniscule");
+  }
+   
+   
+   
+ return 0; 
+   
+}
