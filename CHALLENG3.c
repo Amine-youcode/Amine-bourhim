03aca3bb710d8abbb057d868ec257d280b027738
Amine-youcode1 <bourhymamine@gmail.com>
2024-09-25 12:36:06 +0200

    CHALLENGE3

---
+++ CHALLENG3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    float x;
+    float y;
+    printf("entrer numbr1: ");
+    scanf("%f",&x);
+     printf("entrer numbr2: ");
+    scanf("%f",&y);
+    if (x==y){
+        printf("numbre1+number2= %.2f ",(x+y)*3);
+    }
+    else {
+        printf("numbre1+number2= %.2f ",x + y);
+    }
+    
+    
+
+      
+     
+ 
+   
+    
+   
+     
+}
